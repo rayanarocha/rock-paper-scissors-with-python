856b77865517328def56d47407221aac5f758ad0
Rayana Rocha <rayana.azus@gmail.com>
2022-08-03 20:27:49 -0300

    Layout completo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock-paper-scissors-with-python
 Jogo "Pedra, papel e tesoura" com interface gráfica em Python
+
+# Ícones
+https://icons8.com/
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
+from PIL import Image, ImageTk
+
 # cores --------------------------------
 co0 = "#FFFFFF"  # white / branca
 co1 = "#333333"  # black / preta
@@ -31,6 +33,49 @@ estilo.theme_use('clam')
 
 #configurando o frame_cima
 app_1 = Label(frame_cima, text='Você', height=1, anchor='center', font=('Ivy 10 bold'), bg=co1, fg=co0)
-app_1.place(x=0, y=70)
+app_1.place(x=25, y=70)
+
+app_1_linha = Label(frame_cima, text='', height=10, anchor='center', font=('Ivy 10 bold'), bg=co0, fg=co0)
+app_1_linha.place(x=0, y=0)
+
+app_1_pontos = Label(frame_cima, text='0', height=1, anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_1_pontos.place(x=50, y=20)
+
+app_ = Label(frame_cima, text=':', height=1, anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_.place(x=125, y=20)
+
+app_2_pontos = Label(frame_cima, text='0', height=1, anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_2_pontos.place(x=170, y=20)
+
+app_2 = Label(frame_cima, text='PC', height=1, anchor='center', font=('Ivy 10 bold'), bg=co1, fg=co0)
+app_2.place(x=205, y=70)
+
+app_2_linha = Label(frame_cima, text='', height=10, anchor='center', font=('Ivy 10 bold'), bg=co0, fg=co0)
+app_2_linha.place(x=255, y=0)
+
+app_linha = Label(frame_cima, text='', width=255, anchor='center', font=('Ivy 1 bold'), bg=co0, fg=co0)
+app_linha.place(x=0, y=95)
+
+#configurando o frame_baixo
+icon_1 = Image.open('images/pedra.png')
+icon_1 = icon_1.resize((50, 50), Image.ANTIALIAS)
+icon_1 = ImageTk.PhotoImage(icon_1)
+b_icon_1 = Button(frame_baixo, width=50, image=icon_1, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief=FLAT)
+b_icon_1.place(x=15, y=60)
+
+icon_2 = Image.open('images/papel.png')
+icon_2 = icon_2.resize((50, 50), Image.ANTIALIAS)
+icon_2 = ImageTk.PhotoImage(icon_2)
+b_icon_2 = Button(frame_baixo, width=50, image=icon_2, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief=FLAT)
+b_icon_2.place(x=95, y=60)
+
+icon_3 = Image.open('images/tesoura.png')
+icon_3 = icon_3.resize((50, 50), Image.ANTIALIAS)
+icon_3 = ImageTk.PhotoImage(icon_3)
+b_icon_3 = Button(frame_baixo, width=50, image=icon_3, compound=CENTER, bg=co0, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief=FLAT)
+b_icon_3.place(x=170, y=60)
+
+b_jogar = Button(frame_baixo, width=30, text='Jogar', bg=fundo, fg=co0, font=('Ivy 10 bold'), anchor=CENTER, relief=RAISED, overrelief=RIDGE)
+b_jogar.place(x=5, y=151)
 
 janela.mainloop()
